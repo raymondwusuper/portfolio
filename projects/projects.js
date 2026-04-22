0e35f4d36991bc577c70f8fc59aed0bbc9241bcc
raymondwusuper <raymond.wu.super@gmail.com>
2026-04-22 13:17:48 -0700

    s

---
+++ projects/projects.js
@@ -0,0 +1,11 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+
+const projectsTitle = document.querySelector('.projects-title');
+if (projectsTitle) {
+  projectsTitle.textContent = `${projects.length} Projects`;
+}
+
+const projectsContainer = document.querySelector('.projects');
+renderProjects(projects, projectsContainer, 'h2');
