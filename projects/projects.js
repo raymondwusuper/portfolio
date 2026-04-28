0ae44ff9caf648c85a9473a8365dfc0c6292bfe5
raymondwusuper <raymond.wu.super@gmail.com>
2026-04-27 19:43:15 -0700

    s

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 
